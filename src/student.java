d47fbbdda48817811c6ed681181f277524be611a
alpha <alphdahir@gmail.com>
2022-03-08 15:35:01 -0800

    bit practices

--- src/student.java
+++ src/student.java
@@ -1,16 +1,14 @@
 public class student
 {
-    public void tesla(){
-        System.out.println("my car is going as fast as it can ");
-    }
-    public void speed(int max_speed){
-        System.out.println("the speed limit is "+max_speed);
+    int x ;
+    public student(){
+        x= 5;
 
     }
     public static void main(String[] orgs){
-        student car = new student();
-        car.tesla();
-        car.speed(200);
+        student ob = new student();
+        System.out.println("condstract intailing calling method "+ob.x);
     }
+
 }
 
