a9047b154109a045646db930647e7d2b96a6f2a3
alpha <alphdahir@gmail.com>
2022-03-08 21:07:36 -0800

    bit practices

--- src/student.java
+++ src/student.java
@@ -1,14 +1,15 @@
-public class student
-{
-final int p1 = 20;
-final float p2 =30;
-public static void main(String[] orgs){
-    student ob = new student();
+public class student{
+ static void myStaticMothord(){
+     System.out.println("this is my static methord and can acess with out object ");
+ }
+ public void myPublicMethod(){
+     System.out.println("this is my public method and can only access with object");
+ }
 
-    System.out.println("cant modifiied since its final "+ob.p1);
-    System.out.println("cant modified since its final modifier "+ob.p2);
-}
-
-
+    public static void main(String[] orgs){
+     student ob = new student();
+     ob.myPublicMethod();
+     myStaticMothord();
+    }
 }
 
